c41f5c47ca641d0bfb1b97479dc94ac25e413d70
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-06 22:28:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
+let inpuT =document.getElementById("newTodoInput");
+let button =document.getElementById("addTodoBtn");
+let todo = document.getElementById("todoList");
 
+button.addEventListener("click", function(){
+	let input = inpuT.value.trim();
+	if(input !== "" )
+	{
+		const Li = document.createElement("li");
+		Li.textContent = input;
+
+		todo.appendChild(Li);
+
+		inpuT.value = "";
+	}
+});
